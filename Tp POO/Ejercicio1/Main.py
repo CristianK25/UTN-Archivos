ba9522673ff75d945705d96c01f39d56b3cc24ba
CristianK25 <krahulikcristian@gmail.com>
2024-10-29 18:58:52 -0300

    casi echo

---
+++ Tp POO/Ejercicio1/Main.py
@@ -0,0 +1,21 @@
+import Matriz,Celda
+def main():
+    matriz  = Matriz()
+    while(True):
+        if (ingresarValores(matriz)):
+            break
+    matriz.mostrarCeldas()           
+
+    
+def ingresarValores(matriz):
+    valor = input("Ingrese el valor: ")
+    if (valor ==  "FIN"):
+        return True
+    fila = int(input("Ingrese la fila: "))
+    columna = int(input("Ingrese la columna: "))
+    matriz.ingresarCeldas(fila,columna,valor)
+    return  False
+
+
+if __name__ == "__main__":
+    main()

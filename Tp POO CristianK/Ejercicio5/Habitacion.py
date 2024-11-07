8c2231d38e9f488383aaf5c3e131d7ac98c1cbf2
CristianK25 <krahulikcristian@gmail.com>
2024-11-07 20:17:47 -0300

    ejercicio 5 terminado

---
+++ Tp POO CristianK/Ejercicio5/Habitacion.py
@@ -0,0 +1,4 @@
+class Habitacion:
+    def __init__(self, nombre, metrosCuadrados):
+        self.nombre = nombre
+        self.metrosCuadrados = metrosCuadrados
--- Tp POO CristianK/punto5TP POO/clase_habitacion.py
+++
@@ -1,4 +0,0 @@
-class Habitacion:
-    def __init__(self, nombre: str, metrosCuadrados: float):
-        self.nombre = nombre
-        self.metrosCuadrados = metrosCuadrados

ca5b1850e196be782a18f64a90470c3262e6bdc8
CristianK25 <krahulikcristian@gmail.com>
2025-03-13 20:02:02 -0300

    Casi terminado

--- Programacion 2/UML/src/PaisesProvinciasCiudades/Provincia.java
+++ Programacion 2/UML/src/PaisesProvinciasCiudades/Provincia.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class Provincia {
     private String nombre;
+    private Ciudad capital;
     public ArrayList<Ciudad> ciudades = new ArrayList<>();
+    public ArrayList<Provincia> provinciasLimite = new ArrayList<>();
+    public ArrayList<Pais> paisesLimite = new ArrayList<>();
 
 
     public String getNombre() {

6241b998b6e73775de590f91b8455e8c7192cfd2
CristianK25 <krahulikcristian@gmail.com>
2025-03-27 14:49:08 -0300

    cambios bd y domizio

---
+++ Programacion 2/Programacion Domizio/Cajero_Automatico.java
@@ -0,0 +1,35 @@
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import java.lang.management.GarbageCollectorMXBean;
+
+public class Cajero_Automatico {
+    //Atributos
+    public final double efectivo = 10;
+    protected static boolean acepta;
+    private String devuelve;
+
+
+
+    //Metodos
+    public boolean isAcepta() {
+        return acepta;
+    }
+
+
+    public String getDevuelve() {
+        return devuelve;
+    }
+
+    public void setDevuelve(String devuelve) {
+        this.devuelve = devuelve;
+    }
+
+
+    public void procesarPedido(){
+    }
+
+    public boolean pago(double pago){
+        return false;
+    }
+
+}
